c12152996fb88630a408a2bbbe3dee9e3d7a8245
George9000 <thomaspg976@gmail.com>
2024-04-06 20:17:45 -0400

    Reboot start

---
+++ analyze.jl
@@ -0,0 +1,113 @@
+## Setup ##
+using flights, DataFrames, Chain, AnalysisUtils, DataSkimmer, UnicodePlots, Deneb, CairoMakie
+using MethodChains, PartialFuns
+MethodChains.init_repl()
+PartialFuns.init_repl()
+
+const inputp = "../flightdata"
+readdir(inputp)[occursin.(r"\.csv$", readdir(inputp))]
+const aaarrivals = "american_arrivals.csv"
+const aadepartures = "american_departures.csv"
+const deltaarrivals = "delta_arrivals.csv"
+const deltadepartures = "delta_departures.csv"
+const jbarrivals = "jetblue_arrivals.csv"
+const jbdepartures = "jetblue_departures.csv"
+const unarrivals = "united_arrivals.csv"
+const undepartures = "united_departures.csv"
+const plotp = "./plots"
+
+## Look at files ##
+peekfile(inputp, aaarrivals, n = 12)
+peekfile(inputp, aaarrivals, n = 12, rev = true)
+
+## Read American Airlines Arrivals ##
+readarrival(inputp, aaarrivals, version = 3) |> describe
+# this shows multiple errors, despite attempts to describe time types
+# look at error message and try to find error rows, accounting for fuzziness of line references given multithreading
+# show(readarrival(inputp, aaarrivals)[11100:11300, [7, 11]], allrows = true)
+# all errors show up in cols 7 and 11
+# Show rows of columns 7 and 11 where there is a 24:00 for time
+@chain readarrival(inputp, aaarrivals) begin
+    subset([7, 11] => ByRow((x, y) -> x == "24:00" || y == "24:00"))
+end
+# correct readarrival function
+readarrival(inputp, aaarrivals, version = 4) |> describe
+readarrival(inputp, aaarrivals, version = 4)
+
+# @chain readarrival(inputp, aaarrivals, version = 4) begin
+#     @aside show(stdout, "text/plain", skim(_))
+#     pageddf(1, 20, 5)
+# end
+
+# Description of the American arrivals dataset
+readarrival(inputp, aaarrivals, version = 4).{
+    show(stdout, "text/plain", skim(it))    pageddf(it, 1, 20, 5)
+    them
+}
+
+# Now read all available arrival files of carriers
+readallarrival(inputp, [aaarrivals, deltaarrivals, jbarrivals, unarrivals]).{
+    show(stdout, "text/plain", skim(it))    pageddf(it, 1, 20, 5)
+    them
+}
+# What is the most frequent tail_number?
+readallarrival(inputp, [aaarrivals, deltaarrivals, jbarrivals, unarrivals]).{
+    groupby(it, :tail_number)           groupby(it, :origin_airport)
+    combine(it, nrow)                   combine(it, nrow)
+    sort(it, :nrow, rev = true)         sort(it, :nrow, rev = true)
+    them
+}
+
+readallarrival(inputp, [aaarrivals, deltaarrivals, jbarrivals, unarrivals]).{
+    groupby(it, :tail_number)               groupby(it, :origin_airport)
+    combine(it, nrow)                       combine(it, nrow)
+    sort(it, :nrow, rev = true)             sort(it, :nrow, rev = true)
+    histogram(it.nrow, title = "Tails")     histogram(it.nrow, title = "Origins")
+    them
+}
+
+readallarrival(inputp, [aaarrivals, deltaarrivals, jbarrivals, unarrivals]).{
+    histogram(it.arrival_delay_minutes, title = "Arrival Delay, min")
+}
+
+readallarrival(inputp, [aaarrivals, deltaarrivals, jbarrivals, unarrivals]).{
+    groupby(it, :carrier_code)
+    [histogram(g.arrival_delay_minutes, title = "Arrival Delay, min") for g in it]
+}
+
+storearrivals(inputp, [aaarrivals, deltaarrivals, jbarrivals, unarrivals], "allarrivals.serial")
+
+retrievearrivals(inputp, "allarrivals.serial").{
+    Data(it) * Mark(:bar) * Facet(row=:carrier_code) * Encoding(
+        x=field("arrival_delay_minutes:Q", bin=(;maxbins=50)),
+        y="count()")
+}
+
+retrievearrivals(inputp, "allarrivals.serial").{
+rainclouds(it.carrier_code, it.arrival_delay_minutes;
+    xlabel = "Carriers", ylabel = "Arrival delay, min",
+    plot_boxplots = true, cloud_width=0.5, clouds=hist, hist_bins=100,
+    xticks = ([1, 2, 3, 4], string.(sort(unique(it.carrier_code)))),
+    color = Makie.wong_colors()[indexin(it.carrier_code, sort(unique(it.carrier_code)))])
+}
+
+retrievearrivals(inputp, "allarrivals.serial").{
+    describe(it)
+    # string.(sort(unique(it.carrier_code)))
+}
+
+
+retrievearrivals(inputp, "allarrivals.serial").{
+rainclouds(it.carrier_code, it.arrival_delay_minutes;
+    xlabel = "Carriers", ylabel = "Arrival delay, min",
+    plot_boxplots = true, cloud_width=0.5, clouds=hist, hist_bins=100,
+    xticks = ([1, 2, 3, 4], string.(sort(unique(it.carrier_code)))),
+    color = Makie.wong_colors()[indexin(it.carrier_code, sort(unique(it.carrier_code)))])
+}
+
+# Before running, change the font string to a monospaced font found on your system
+retrievearrivals(inputp, "allarrivals.serial").{
+    arrivaldelayviol(it, plotp, "arrival_delay_minutes.png"; font = "TheSansMono-W5Regular")
+}
+
+exit()
---
+++ src/flights.jl
@@ -0,0 +1,101 @@
+module flights
+using CSV, DataFrames, CategoricalArrays, Dates, Missings, Serialization
+using UnicodePlots, Statistics, StatsBase, FreqTables, Chain
+using AnalysisUtils, MethodChains
+import TableTransforms: StdNames
+using CairoMakie, ColorSchemes
+export readarrival, readallarrival, storearrivals, retrievearrivals
+export arrivaldelayviol
+
+"""
+    readarrival(inputpath, filename)
+
+Read in a flight arrivals and process it to a DataFrame.
+"""
+function readarrival(inputpath, filename; version=1)
+    input = joinpath(inputpath, filename)
+    if version == 1
+        CSV.read(input, DataFrame; header = 8, footerskip = 2)
+    elseif version == 2
+        CSV.read(input, DataFrame; header = 8, footerskip = 2,
+            dateformat = Dict(
+                2 => dateformat"m/d/y",
+                6 => dateformat"H:M",
+                7 => dateformat"H:M",
+                11 => dateformat"H:M"))
+    elseif version == 3
+        CSV.read(input, DataFrame; header = 8, footerskip = 2,
+            types = Dict(
+                2 => Date,
+                6 => Time,
+                7 => Time,
+                11 => Time),
+            dateformat = Dict(
+                2 => dateformat"m/d/y",
+                6 => dateformat"H:M",
+                7 => dateformat"H:M",
+                11 => dateformat"H:M"))
+    else
+        df = CSV.read(input, DataFrame; dateformat = "m/d/y", header = 8, footerskip = 2)
+        transform!(df, [7, 11] .=> ByRow(str -> replace(str, "24:00" => "00:00")), renamecols = false)
+        transform!(df, [6, 7, 11] .=> ByRow(str -> Time(str, dateformat"H:M")), renamecols = false)
+        return df
+    end
+end
+
+"""
+    readallarrival(inputpath, filenamesvector)
+
+Read in a set of arrivals flight data files and concatenate them into one dataframe.
+"""
+function readallarrival(inputpath, filenamesvector)
+    resultdf = DataFrame()
+    for f in filenamesvector
+        input = joinpath(inputpath, f)
+        @mc CSV.read(input, DataFrame; dateformat = "m/d/y", header = 8, footerskip = 2).{
+            transform!(it, [7, 11] .=> ByRow(str -> replace(str, "24:00" => "00:00")), renamecols = false)
+            transform!(it, [6, 7, 11] .=> ByRow(str -> Time(str, dateformat"H:M")), renamecols = false)
+            resultdf = vcat(it, resultdf)
+        }
+    end
+    return resultdf |> StdNames(:snake)
+end
+
+"""
+    storearrivals(inputpath, infilename, outfilename)
+
+Store all arrivals in a serial file.
+"""
+function storearrivals(inputpath, filenamesvector, outfilename)
+    return serialize(joinpath(inputpath, outfilename), readallarrival(inputpath, filenamesvector))
+end
+
+"""
+    retrievearrivals(inputpath, filename)
+
+Deserialize arrivals serial file.
+"""
+function retrievearrivals(inputpath, filename)
+    return deserialize(joinpath(inputpath, filename))
+end
+
+"""
+    arrivaldelayviol(df, ppath, pname; font = "TheSansMono-W5Regular")
+
+Plot violin plots of distribution of arrival delay in minutes for each carrier.
+"""
+function arrivaldelayviol(df, ppath, pname; font = "TheSansMono-W5Regular")
+    categories = string.(sort(unique(df.carrier_code)))
+    colors = categorical_colors(:Set1, length(categories))
+    fig = Figure(size = (900, 600), fontsize = 14, fonts = (; regular = font))
+    ax = Axis(fig[1, 1], xticks = (1:length(categories), categories))
+    for (indx, f) in enumerate(categories)
+        delaym = subset(df, :carrier_code => ByRow(==(f)))[:, :arrival_delay_minutes]
+        carrier = fill(indx, length(delaym))
+        violin!(ax, carrier, delaym; width = 0.35, color = (colors[indx], 0.45), strokecolor = colors[indx], show_median = true, mediancolor = :black)
+    end
+    save(joinpath(ppath, pname), fig; px_per_unit = 3)
+end
+
+
+end # module flights
